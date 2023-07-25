9356056a084d04fbcaa3f1d6e94624e8dd502504
Ayomide Arekemase <mideareke@gmail.com>
2023-07-25 02:31:50 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4eae2a620fmsh76531e5c4bd51d7p11eea0jsn8fab3bd81eab',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = 'd3d6cb9e99a3f9d08a0c600809ce47fa'
+
